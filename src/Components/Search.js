c3dba172951015f44cd8168b21858713a5b4d42c
brian4548 <brianr4548@gmail.com>
2021-08-03 15:38:22 -0500

    added search event

---
+++ src/Components/Search.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+const Search = () => {
+
+    const [term, setTerm] = useState('')
+
+    const handleChange = (event) => {
+        setTerm(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    return(
+        <div>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Enter Search Term</label>
+                    <input  value={term} onChange={handleChange}className='input' type='text'></input>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
