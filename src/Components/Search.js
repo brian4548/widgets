b59733e7029bb1fbcbe89056a1628d7d49a01bfe
brian4548 <brianr4548@gmail.com>
2021-08-15 23:53:10 -0500

    changed the default term to a conditional and setResults to query and search.

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 const Search = () => {
-  const [term, setTerm] = useState('tesla');
+  const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
 
   console.log(results)
@@ -19,11 +19,13 @@ const Search = () => {
         }
       });
 
-      setResults(data);
+      setResults(data.query.search);
       
     }
 
-    search()
+    if (term){
+      search()
+    }
   }, [term])
 
   const handleChange = (event) => {
