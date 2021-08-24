2e3256b94e72e5d0995af672af07824686ae56fb
brian4548 <brianr4548@gmail.com>
2021-08-24 18:44:26 -0500

    made it so you can close dropdown by clicking anywhere on the screen

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener("click", () => {
+      setOpen(false);
+    }, {capture: true});
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -21,17 +27,22 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   const openDropdown = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   return (
     <div className="ui form">
       <div className="field">
         <label className="label">select a color </label>
-        <div onClick={openDropdown} className={`ui selection dropdown ${open ? 'visible ative' : ''}`}>
+        <div
+          onClick={openDropdown}
+          className={`ui selection dropdown ${open ? "visible ative" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
