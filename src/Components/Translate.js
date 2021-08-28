775931aae7974c7dd0a401a87942e1b0f2bb3785
brian4548 <brianr4548@gmail.com>
2021-08-28 17:06:17 -0500

    added text field and language select dropdown

--- src/Components/Translate.js
+++ src/Components/Translate.js
@@ -3,7 +3,7 @@ import Dropdown from "./Dropdown";
 
 const options = [
   {
-    language: "Afrikaans",
+    label: "Afrikaans",
     value: "af",
   },
   {
@@ -18,12 +18,26 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input onChange={handleTextChange} value={text} />
+        </div>
+      </div>
+
       <Dropdown
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
+        label="select a language"
       />
     </div>
   );
